438236e76547150e1553509a3fb1254e6e0462ec
ehsangh7 <ehsanghochagh@gmail.com>
2021-01-09 04:14:18 +0330

    add animation to pages from animations folder

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -2,11 +2,17 @@ import React from 'react';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 import Image from '../images/img-3.jpg';
-
+import { motion } from 'framer-motion';
+import { animationThree } from '../animations';
 
 const Services = () => {
   return (
-    <>
+    <motion.div
+      initial='out'
+      animate='end'
+      exit='out'
+      variants={animationThree}
+    >
       <Header />
       <Hero
         image={Image}
@@ -14,7 +20,7 @@ const Services = () => {
         desc='Can you believe this?'
       />
       Services
-    </>
+    </motion.div>
   );
 };
 
