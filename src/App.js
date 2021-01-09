c03989f49592faee0f4cff954a654b2bd2b85754
ehsangh7 <ehsanghochagh@gmail.com>
2021-01-09 04:01:30 +0330

    adding Hero and Header to route pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About';
+import Services from './pages/Services';
+
 
 function App() {
   return (
-   <>
-    yoooo
-   </>
+   <Router>
+     <Switch>
+       <Route path="/" component={Home}/>
+       <Route path="/About" component={About}/>
+       <Route path="/Services" component={Services}/>
+     </Switch>
+ 
+   </Router>
   );
 }
 
